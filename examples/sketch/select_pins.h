fa192f1bc93d8292ac9c55ef7af294f97e5e6a0e
Micky <513673326@qq.com>
2022-11-22 14:23:14 +0800

    Add CAMERA_MODEL_TTGO_T_CAM_SIM pin mapping

--- examples/sketch/select_pins.h
+++ examples/sketch/select_pins.h
@@ -255,6 +255,32 @@ Compatible with all TTGO camera products, written by LewisHe
 #define PCLK_GPIO_NUM    25
 
 #define BUTTON_1         0
+#elif defined(CAMERA_MODEL_TTGO_T_CAM_SIM)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM 18
+#define XCLK_GPIO_NUM  14
+#define SIOD_GPIO_NUM  4
+#define SIOC_GPIO_NUM  5
+
+#define Y9_GPIO_NUM    15
+#define Y8_GPIO_NUM    16
+#define Y7_GPIO_NUM    17
+#define Y6_GPIO_NUM    12
+#define Y5_GPIO_NUM    10
+#define Y4_GPIO_NUM    8
+#define Y3_GPIO_NUM    9
+#define Y2_GPIO_NUM    11
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM  7
+#define PCLK_GPIO_NUM  13
+
+#define PWR_ON_PIN     1
+#define PCIE_PWR_PIN   48
+#define PCIE_RST_PIN   48
+#define PCIE_TX_PIN    45
+#define PCIE_RX_PIN    46
+#define PCIE_LED_PIN   21
 #else
+
 #error "Please select the model of the board you want to use in main.cpp"
 #endif
--- examples/sketch/sketch.ino
+++ examples/sketch/sketch.ino
@@ -26,26 +26,25 @@ Compatible with all TTGO camera products, written by LewisHe
 // #define T_Camera_V162_VERSION
 // #define T_Camera_V17_VERSION
 // #define ESPRESSIF_ESP_EYE
+// #define CAMERA_MODEL_TTGO_T_CAM_SIM
 
 /***************************************
  *  Function
  **************************************/
 // #define SOFTAP_MODE       //The comment will be connected to the specified ssid
 
-//When there is BME280, set the reading time here
-#define DEFAULT_MEASUR_MILLIS   3000        /* Get sensor time by default (ms)*/
+// When there is BME280, set the reading time here
+#define DEFAULT_MEASUR_MILLIS 3000 /* Get sensor time by default (ms)*/
 
-//When using timed sleep, set the sleep time here
-#define uS_TO_S_FACTOR 1000000              /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  5                    /* Time ESP32 will go to sleep (in seconds) */
-
+// When using timed sleep, set the sleep time here
+#define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 5        /* Time ESP32 will go to sleep (in seconds) */
 
 /***************************************
  *  WiFi
  **************************************/
-#define WIFI_SSID   "YourSSID"
+#define WIFI_SSID "YourSSID"
 #define WIFI_PASSWD "YourPASSWORD"
-
 
 #include "select_pins.h"
 
@@ -57,11 +56,10 @@ String ipAddress = "";
 extern void startCameraServer();
 
 #if defined(BUTTON_1)
-//Depend BME280 library ,See https://github.com/mathertel/OneButton
+// Depend BME280 library ,See https://github.com/mathertel/OneButton
 #include <OneButton.h>
 OneButton button(BUTTON_1, true);
 #endif
-
 
 #if defined(ENABLE_BEM280)
 // Depend BME280 library ,See https://github.com/sparkfun/SparkFun_BME280_Arduino_Library
@@ -89,16 +87,17 @@ bool setupSensor()
 {
 #if defined(ENABLE_BEM280)
     bool status = sensor.beginI2C();
-    if (!status)return false;
-    sensor.setMode(MODE_SLEEP); //Sleep for now
-    sensor.setFilter(1); //0 to 4 is valid. Filter coefficient. See 3.4.4
-    sensor.setStandbyTime(0); //0 to 7 valid. Time between readings. See table 27.
+    if (!status)
+        return false;
+    sensor.setMode(MODE_SLEEP); // Sleep for now
+    sensor.setFilter(1);        // 0 to 4 is valid. Filter coefficient. See 3.4.4
+    sensor.setStandbyTime(0);   // 0 to 7 valid. Time between readings. See table 27.
 
-    sensor.setTempOverSample(1); //0 to 16 are valid. 0 disables temp sensing. See table 24.
-    sensor.setPressureOverSample(1); //0 to 16 are valid. 0 disables pressure sensing. See table 23.
-    sensor.setHumidityOverSample(1); //0 to 16 are valid. 0 disables humidity sensing. See table 19.
+    sensor.setTempOverSample(1);     // 0 to 16 are valid. 0 disables temp sensing. See table 24.
+    sensor.setPressureOverSample(1); // 0 to 16 are valid. 0 disables pressure sensing. See table 23.
+    sensor.setHumidityOverSample(1); // 0 to 16 are valid. 0 disables humidity sensing. See table 19.
 
-    sensor.setMode(MODE_SLEEP); //MODE_SLEEP, MODE_FORCED, MODE_NORMAL is valid. See 3.3
+    sensor.setMode(MODE_SLEEP); // MODE_SLEEP, MODE_FORCED, MODE_NORMAL is valid. See 3.3
 #endif
 
 #if defined(AS312_PIN)
@@ -113,20 +112,23 @@ void readSensor()
 
     static long lastMillis;
 
-    if (millis() - lastMillis < DEFAULT_MEASUR_MILLIS)return;
-    sensor.setMode(MODE_FORCED); //Wake up sensor and take reading
+    if (millis() - lastMillis < DEFAULT_MEASUR_MILLIS)
+        return;
+    sensor.setMode(MODE_FORCED); // Wake up sensor and take reading
 
     long startTime = millis();
-    while (sensor.isMeasuring() == false) ; //Wait for sensor to start measurment
-    while (sensor.isMeasuring() == true) ; //Hang out while sensor completes the reading
+    while (sensor.isMeasuring() == false)
+        ; // Wait for sensor to start measurment
+    while (sensor.isMeasuring() == true)
+        ; // Hang out while sensor completes the reading
     long endTime = millis();
 
-    //Sensor is now back asleep but we get get the data
+    // Sensor is now back asleep but we get get the data
     Serial.print(" Measure time(ms): ");
     Serial.print(endTime - startTime);
 
     Serial.print(" Humidity: ");
-    humidity  = sensor.readFloatHumidity();
+    humidity = sensor.readFloatHumidity();
     Serial.print(humidity);
 
     Serial.print(" Pressure: ");
@@ -134,7 +136,7 @@ void readSensor()
     Serial.print(pressure);
 
     Serial.print(" Alt: ");
-    //Serial.print(sensor.readFloatAltitudeMeters(), 1);
+    // Serial.print(sensor.readFloatAltitudeMeters(), 1);
     altitude = String(sensor.readFloatAltitudeFeet());
     Serial.print(altitude);
 
@@ -145,10 +147,8 @@ void readSensor()
 
     Serial.println();
 
-#endif  /*ENABLE_BEM280*/
+#endif /*ENABLE_BEM280*/
 }
-
-
 
 bool deviceProbe(uint8_t addr)
 {
@@ -174,7 +174,7 @@ bool setupDisplay()
 
 #elif defined(SSD130_MODLE_TYPE)
     static FrameCallback frames[] = {
-        [](OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y)
+        [](OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
         {
             display->setTextAlignment(TEXT_ALIGN_CENTER);
             display->setFont(ArialMT_Plain_10);
@@ -187,14 +187,13 @@ bool setupDisplay()
 #endif
 
 #if defined(AS312_PIN)
-            if (digitalRead(AS312_PIN)) {
+            if (digitalRead(AS312_PIN))
+            {
                 display->drawString(64 + x, 40 + y, "AS312 Trigger");
             }
 #endif
-
-
         },
-        [](OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y)
+        [](OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
         {
 #if defined(ENABLE_BME280)
             display->setFont(ArialMT_Plain_16);
@@ -207,31 +206,30 @@ bool setupDisplay()
             display->setTextAlignment(TEXT_ALIGN_CENTER);
             display->setFont(ArialMT_Plain_10);
 
-
 #if (SSD130_MODLE_TYPE)
             // if (oled.getHeight() == 32) {
-            display->drawString( 64 + x, 0 + y, "Camera Ready! Use");
-            display->drawString(64 + x, 10 + y, "http://" + ipAddress );
+            display->drawString(64 + x, 0 + y, "Camera Ready! Use");
+            display->drawString(64 + x, 10 + y, "http://" + ipAddress);
             display->drawString(64 + x, 16 + y, "to connect");
-            // } else {
+        // } else {
 #else
-            display->drawString( 64 + x, 5 + y, "Camera Ready! Use");
-            display->drawString(64 + x, 25 + y, "http://" + ipAddress );
+            display->drawString(64 + x, 5 + y, "Camera Ready! Use");
+            display->drawString(64 + x, 25 + y, "http://" + ipAddress);
             display->drawString(64 + x, 45 + y, "to connect");
-            // }
-#endif  /*SSD130_MODLE_TYPE*/
+        // }
+#endif /*SSD130_MODLE_TYPE*/
 
-#endif  /*ENABLE_BME280*/
-        }
-    };
+#endif /*ENABLE_BME280*/
+        }};
 
-    if (!deviceProbe(SSD1306_ADDRESS))return false;
+    if (!deviceProbe(SSD1306_ADDRESS))
+        return false;
     oled.init();
     // Wire.setClock(100000);  //! Reduce the speed and prevent the speed from being too high, causing the screen
     oled.setFont(ArialMT_Plain_16);
     oled.setTextAlignment(TEXT_ALIGN_CENTER);
     // delay(50);
-    oled.drawString( oled.getWidth() / 2, oled.getHeight() / 2 - 10, "LilyGo CAM");
+    oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2 - 10, "LilyGo CAM");
     oled.display();
     ui.setTargetFPS(30);
     ui.setIndicatorPosition(BOTTOM);
@@ -244,33 +242,33 @@ bool setupDisplay()
     return true;
 }
 
-
 void loopDisplay()
 {
 #if defined(SSD130_MODLE_TYPE)
-    if (ui.update()) {
-#endif  /*SSD130_MODLE_TYPE*/
+    if (ui.update())
+    {
+#endif /*SSD130_MODLE_TYPE*/
 
 #if defined(BUTTON_1)
         button.tick();
-#endif  /*BUTTON_1*/
+#endif /*BUTTON_1*/
 
 #if defined(SSD130_MODLE_TYPE)
     }
 #elif defined(ENABLE_TFT)
 
-        // ***
+    // ***
 
-#endif  /*SSD130_MODLE_TYPE & ENABLE_TFT*/
+#endif /*SSD130_MODLE_TYPE & ENABLE_TFT*/
 }
-
 
 bool setupPower()
 {
 #if defined(ENABLE_IP5306)
 #define IP5306_ADDR 0X75
 #define IP5306_REG_SYS_CTL0 0x00
-    if (!deviceProbe(IP5306_ADDR))return false;
+    if (!deviceProbe(IP5306_ADDR))
+        return false;
     bool en = true;
     Wire.beginTransmission(IP5306_ADDR);
     Wire.write(IP5306_REG_SYS_CTL0);
@@ -281,9 +279,10 @@ bool setupPower()
     return Wire.endTransmission() == 0;
 
 #elif defined(ENABLE_AXP192)
-#define AXP192_ADDRESS  0x34
-    if (!deviceProbe(AXP192_ADDRESS))return false;
-    //Turn off no use power channel , Or you can do nothing
+#define AXP192_ADDRESS 0x34
+    if (!deviceProbe(AXP192_ADDRESS))
+        return false;
+    // Turn off no use power channel , Or you can do nothing
     uint8_t val;
     Wire.beginTransmission(AXP192_ADDRESS);
     Wire.write(0x30);
@@ -293,14 +292,14 @@ bool setupPower()
 
     Wire.beginTransmission(AXP192_ADDRESS);
     Wire.write(0x30);
-    Wire.write( val & 0xFC);
+    Wire.write(val & 0xFC);
     Wire.endTransmission();
 
     Wire.beginTransmission(AXP192_ADDRESS);
     Wire.write(0x12);
     Wire.endTransmission();
     Wire.requestFrom(AXP192_ADDRESS, 1);
-    val =  Wire.read();
+    val = Wire.read();
 
     Wire.beginTransmission(AXP192_ADDRESS);
     Wire.write(0x12);
@@ -329,12 +328,15 @@ bool setupSDCard()
     // SPI.begin(TFT_SCLK_PIN, TFT_MISO_PIN, TFT_MOSI_PIN);
 
 #if defined(SDCARD_CS_PIN)
-    if (!SD.begin(SDCARD_CS_PIN)) {
+    if (!SD.begin(SDCARD_CS_PIN))
+    {
         tft.setTextColor(TFT_RED);
         tft.drawString("SDCard begin failed", tft.width() / 2, tft.height() / 2 - 20);
         tft.setTextColor(TFT_WHITE);
         return false;
-    } else {
+    }
+    else
+    {
         String cardInfo = String(((uint32_t)SD.cardSize() / 1024 / 1024));
         tft.setTextColor(TFT_GREEN);
         tft.drawString("SDcardSize=[" + cardInfo + "]MB", tft.width() / 2, tft.height() / 2 + 92);
@@ -347,7 +349,6 @@ bool setupSDCard()
 #endif
     return true;
 }
-
 
 bool setupCamera()
 {
@@ -376,17 +377,20 @@ bool setupCamera()
     config.pixel_format = PIXFORMAT_JPEG;
     config.fb_location = CAMERA_FB_IN_PSRAM;
     config.grab_mode = CAMERA_GRAB_LATEST;
-    //init with high specs to pre-allocate larger buffers
-    if (psramFound()) {
+    // init with high specs to pre-allocate larger buffers
+    if (psramFound())
+    {
         config.frame_size = FRAMESIZE_UXGA;
         config.jpeg_quality = 10;
         config.fb_count = 2;
-    } else {
+    }
+    else
+    {
         config.frame_size = FRAMESIZE_SVGA;
         config.jpeg_quality = 12;
         config.fb_count = 1;
         config.fb_location = CAMERA_FB_IN_DRAM;
-   }
+    }
 #endif
 
 #if defined(ESPRESSIF_ESP_EYE) || defined(T_Camera_V162_VERSION) || defined(T_Camera_MINI_VERSION)
@@ -399,22 +403,24 @@ bool setupCamera()
 
     // camera init
     esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK) {
+    if (err != ESP_OK)
+    {
         Serial.printf("Camera init failed with error 0x%x\n", err);
         return false;
     }
 
     sensor_t *s = esp_camera_sensor_get();
-    //initial sensors are flipped vertically and colors are a bit saturated
-    if (s->id.PID == OV3660_PID) {
-        s->set_vflip(s, 1);//flip it back
-        s->set_brightness(s, 1);//up the blightness just a bit
-        s->set_saturation(s, -2);//lower the saturation
+    // initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID)
+    {
+        s->set_vflip(s, 1);       // flip it back
+        s->set_brightness(s, 1);  // up the blightness just a bit
+        s->set_saturation(s, -2); // lower the saturation
     }
-    //drop down frame size for higher initial frame rate
+    // drop down frame size for higher initial frame rate
     s->set_framesize(s, FRAMESIZE_QVGA);
 
-#if  defined(T_Camera_V162_VERSION)
+#if defined(T_Camera_V162_VERSION)
     s->set_vflip(s, 1);
     s->set_hmirror(s, 1);
 #endif
@@ -431,7 +437,8 @@ void setupNetwork()
     ipAddress = WiFi.softAPIP().toString();
 #else
     WiFi.begin(WIFI_SSID, WIFI_PASSWD);
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(500);
         Serial.print(".");
     }
@@ -451,22 +458,27 @@ void setupNetwork()
 void setupButton()
 {
 #if defined(BUTTON_1)
-    button.attachClick([]() {
-        static bool en = false;
-        sensor_t *s = esp_camera_sensor_get();
-        en = en ? 0 : 1;
-        s->set_vflip(s, en);
+    button.attachClick([]()
+                       {
+                           static bool en = false;
+                           sensor_t *s = esp_camera_sensor_get();
+                           en = en ? 0 : 1;
+                           s->set_vflip(s, en);
 #if defined(SSD130_MODLE_TYPE)
-        if (en) {
-            oled.resetOrientation();
-        } else {
-            oled.flipScreenVertically();
-        }
+                           if (en)
+                           {
+                               oled.resetOrientation();
+                           }
+                           else
+                           {
+                               oled.flipScreenVertically();
+                           }
 #endif
-        // delay(200);
-    });
+                           // delay(200);
+                       });
 
-    button.attachDoubleClick([]() {
+    button.attachDoubleClick([]()
+                             {
         if (PWDN_GPIO_NUM > 0) {
             pinMode(PWDN_GPIO_NUM, PULLUP);
             digitalWrite(PWDN_GPIO_NUM, HIGH);
@@ -492,8 +504,7 @@ void setupButton()
         oled.displayOff();
 #else
         delay(2000);
-#endif  /*SSD130_MODLE_TYPE*/
-
+#endif /*SSD130_MODLE_TYPE*/
 
 #if defined(AS312_PIN) && defined(PIR_SUPPORT_WAKEUP)
         esp_sleep_enable_ext1_wakeup(((uint64_t)(((uint64_t)1) << AS312_PIN)), ESP_EXT1_WAKEUP_ANY_HIGH);
@@ -507,8 +518,7 @@ void setupButton()
 #else
         esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 #endif
-        esp_deep_sleep_start();
-    });
+        esp_deep_sleep_start(); });
 #if defined(T_Camera_MINI_VERSION)
     button.setClickTicks(200);
     button.setDebounceTicks(0);
@@ -516,34 +526,42 @@ void setupButton()
 #endif /*BUTTON_1*/
 }
 
-
-
 void setup()
 {
 
     Serial.begin(115200);
-
+#if defined(PWR_ON_PIN)
+    pinMode(PWR_ON_PIN, OUTPUT);
+    digitalWrite(PWR_ON_PIN, 1);
+#endif
 #if defined(I2C_SDA) && defined(I2C_SCL)
     Wire.begin(I2C_SDA, I2C_SCL);
 #endif
 
     bool status;
     status = setupDisplay();
-    Serial.print("setupDisplay status "); Serial.println(status);
+    Serial.print("setupDisplay status ");
+    Serial.println(status);
 
     status = setupSDCard();
-    Serial.print("setupSDCard status "); Serial.println(status);
+    Serial.print("setupSDCard status ");
+    Serial.println(status);
 
     status = setupPower();
-    Serial.print("setupPower status "); Serial.println(status);
+    Serial.print("setupPower status ");
+    Serial.println(status);
 
     status = setupSensor();
-    Serial.print("setupSensor status "); Serial.println(status);
+    Serial.print("setupSensor status ");
+    Serial.println(status);
 
     status = setupCamera();
-    Serial.print("setupCamera status "); Serial.println(status);
-    if (!status) {
-        delay(10000); esp_restart();
+    Serial.print("setupCamera status ");
+    Serial.println(status);
+    if (!status)
+    {
+        delay(10000);
+        esp_restart();
     }
 
     setupButton();
